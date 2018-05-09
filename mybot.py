3cecd57a414776d0d38405dcbe4ee2d542db1df9
Vipin <vipin.ind.kumar@gmail.com>
2018-05-09 18:11:35 +0530

    add code to get comments

--- mybot.py
+++ mybot.py
@@ -1,9 +1,14 @@
 import praw
 
+# define the credentials of the bot
 bot = praw.Reddit(user_agent='myBot', 
 				  client_id='abc',
 				  client_secret='123',
 				  username='',
 				  password='')
-				  
 
+# add the subreddit name and
+# get a stream of comments			  
+subreddit = bot.subreddit('gifs')
+
+comments = subreddit.stream.comments()
